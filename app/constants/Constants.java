92edd63bcd9083ffac37d206fbab6b5b83cf52a8
Chris Chiappone <chiappone@gmail.com>
2012-01-12 14:31:40 -0700

    Update cassia endpoint

--- app/constants/Constants.java
+++ app/constants/Constants.java
@@ -3,7 +3,7 @@ package constants;
 public class Constants {
 	
 	//public static String WSURL = "https://cassandraio.herokuapp.com/v0.1";
-	public static String WSURL = "http://ec2-107-22-27-252.compute-1.amazonaws.com:9000/v0.1";
+	public static String WSURL = "http://ec2-107-20-28-55.compute-1.amazonaws.com:9000/v0.1";
 	public static String VER = "v0.1";
 	public static String TOKEN = "MlfXEomnR2";
 	public static String ACCOUNTID = "10687d51-966f-4d56-a695-4934ba467f54";
